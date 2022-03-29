8adffa9a89c0041d27ffb4d1655ffb80278b90ad
Your Name <you@example.com>
2022-03-29 23:55:51 +0600

    change sommthing

--- dripukush/zoon_profile/views.py
+++ dripukush/zoon_profile/views.py
@@ -3,117 +3,125 @@ from .models import Zoon_Profile
 from members.models import ZoonList,Members,Office
 from .models import Members,ZoonList,Office
 
-
+#======================dhaka=================
 def Dhaka(request):
     diction={
         'title':'dhaka',
-        'zoon_text':'This is Dhaka Zoon page',
+        'zoon_text':'Dhaka Zoon page',
     }
     return render(request, 'zoon/Dhaka.html', context=diction)
 
 def DhakaMember(request):
-    member_ctg = Members.objects.filter(city__icontains='dhaka')
+    member_dha = Members.objects.filter(city__icontains='dhaka')
     diction={
-        'title':'Dhaka',
-        'zoon_text':'This is Dhaka Zone Member List',
-        'show_list':member_ctg,
+        'title':'dhaka-m',
+        'zoon_text':'Dhaka Zone Member List',
+        'show_list':member_dha,
     }
     return render(request, 'zoon/dhaka/dhaka_mem.html', context=diction)
 
 
 
 
+#======================Chittagong=================
 def Chittagong(request):
     diction={
-        'title':'chittagong',
-        'zoon_text':'This is Chitagong Zoon page',
+        'title':'ctg',
+        'zoon_text':'Chitagong Zoon page',
     }
     return render(request, 'zoon/chittagong.html', context=diction)
 
 def ChittagongMember(request):
     member_ctg = Members.objects.filter(city__icontains='chittagong')
     diction={
-        'title':'chittagong',
-        'zoon_text':'This is Chitagong Zone Member List',
+        'title':'ctg-m',
+        'zoon_text':'Chitagong Zone Member List',
         'show_list':member_ctg,
     }
     return render(request, 'zoon/ctg/ctg_mem.html', context=diction)
 
 
+#======================Comilla=================
 def Cumilla(request):
     member_com = Members.objects.filter(city__contains='comilla')
     diction={
         'title':'cumilla',
-        'zoon_text':'This is Cumilla Zoon page',
+        'zoon_text':'Cumilla Zoon page',
          'show_list':member_com,
     }
     return render(request, 'zoon/cumilla.html', context=diction)
 
 def ComillaMember(request):
-    member_ctg = Members.objects.filter(city__icontains='comilla')
+    member_com = Members.objects.filter(city__icontains='comilla')
     diction={
-        'title':'Comilla',
-        'zoon_text':'This is Comilla Zone Member List',
-        'show_list':member_ctg,
+        'title':'comilla-m',
+        'zoon_text':'Comilla Zone Member List',
+        'show_list':member_com,
     }
     return render(request, 'zoon/comila/com_mem.html', context=diction)
 
 
+
+
+#======================Syllet=================
 def Syllet(request):
     # zone = Members.objects.order_by('')
     member = Members.objects.filter(city__contains='syllet')
     diction={
         'title':'syllet',
-        'zoon_text':'This is syllet Zoon page',
+        'zoon_text':'syllet Zoon page',
         # 'show_list':zone,
         'show_list':member,
     }
     return render(request, 'zoon/syllet.html', context=diction)
 
 def SylletMember(request):
-    member_ctg = Members.objects.filter(city__icontains='Syllet')
+    member_syl = Members.objects.filter(city__icontains='Syllet')
     diction={
-        'title':'Syllet',
-        'zoon_text':'This is Syllet Zone Member List',
-        'show_list':member_ctg,
+        'title':'syllet-m',
+        'zoon_text':'Syllet Zone Member List',
+        'show_list':member_syl,
     }
     return render(request, 'zoon/syllet/syllet_mem.html', context=diction)
 
 
+
+
+#======================Moymonshing=================
 def Moymonshing(request):
     diction={
         'title':'Moymonshing',
-        'zoon_text':'This is Moymonshing Zoon page',
+        'zoon_text':'Moymonshing Zoon page',
     }
     return render(request, 'zoon/moymonshin.html', context=diction)
 
 
 def MoymonshinMember(request):
-    member_ctg = Members.objects.filter(city__icontains='moymonshing')
+    member_moy = Members.objects.filter(city__icontains='moymonshing')
     diction={
-        'title':'Moymonshing',
-        'zoon_text':'This is Moymonshing Zone Member List',
-        'show_list':member_ctg,
+        'title':'moy-m',
+        'zoon_text':'Moymonshing Zone Member List',
+        'show_list':member_moy,
     }
     return render(request, 'zoon/mymoshing/moym_mem.html', context=diction)
 
 
 
 
-
+#======================Power plant=================
 def Power_Plant(request):
     diction={
         'title':'power-Plant',
-        'zoon_text':'This is Power Plant Zoon page',
+        'zoon_text':'Power Plant Zoon page',
     }
     return render(request, 'zoon/Power_plant.html', context=diction)
 
 
 def Power_plantMember(request):
-    member_ctg = Members.objects.filter(city__icontains='Power plant')
+    member_pwp = Members.objects.filter(city__icontains='Power plant')
     diction={
-        'title':'Power-plant',
-        'zoon_text':'Power Zo Plant zone Member List',
-        'show_list':member_ctg,
+        'title':'p-p-m',
+        'zoon_text':'Power Zone Plant zone Member List',
+        'show_list':member_pwp,
     }
     return render(request, 'zoon/power_plnt/pow_plnt_mem.html', context=diction)
